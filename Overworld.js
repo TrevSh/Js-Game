5babfa8e73a73c23ff5dcc6b10edf31ab2990447
TrevSh <103955586+TrevSh@users.noreply.github.com>
2022-05-30 13:16:16 -0500

    Add files via upload

--- Overworld.js
+++ Overworld.js
@@ -3,51 +3,44 @@ class Overworld {
         this.element = config.element;
         this.canvas = this.element.querySelector(".game-canvas");
         this.ctx = this.canvas.getContext("2d");
+        this.map = null;
+    }
+
+//GameLoop!!
+    startGameLoop(){
+        const step = ()=>{
+            
+         //Clears off canvas after animation
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+        //Draw Lower Image
+        this.map.drawLowerImage(this.ctx);
+
+        //Draws Game Objects
+        Object.values(this.map.gameObjects).forEach(object=> {
+           object.update({
+               arrow: this.directionInput.direction
+
+           })
+            object.sprite.draw(this.ctx);
+        })
+
+        //Draws Upper Layer
+        this.map.drawUpperImage(this.ctx);
+
+            requestAnimationFrame(()=> {
+                step();
+            })
+        }
+        step();
     }
 
     //Will be drawn from top to bottom by default
     init(){
-        const image = new Image();
-        image.onload = () => {
-            this.ctx.drawImage(image,0,0)
-        };
-        image.src = "/images/maps/DemoLower.png";   //map
-
-        const x = 5;
-        const y = 6;
-
-        const shadow = new Image();
-        shadow.onload = () => {
-            this.ctx.drawImage(
-                shadow,
-                0,  //left cut
-                0,  //top cut
-                32, //width of cut
-                32, //height of cut                                 //Spritesheet requires 8 values
-                x * 16 - 8,  //x position (*16 to account for grid)
-                y * 16 - 18,  //y position (*16 to account for grid. -18 and -8 to set feet in grid)
-                32, //size
-                32, //size
-            )
-        }
-        shadow.src = "/images/characters/shadow.png";
-
-        const hero = new Image();   //Spritesheet cutting
-        hero.onload = () => {
-            this.ctx.drawImage(
-                hero,
-                0,  //left cut
-                0,  //top cut
-                32, //width of cut
-                32, //height of cut                                 //Spritesheet requires 8 values
-                x * 16 - 8,  //x position (*16 to account for grid)
-                y * 16 - 18,  //y position (*16 to account for grid. -18 and -8 to set feet in grid)
-                32, //size
-                32, //size
-
-         )
-        }
-        hero.src = "/images/characters/people/hero.png" //spritesheet file
+        this.map = new OverworldMap(window.OverworldMaps.DemoRoom);
+        this.directionInput = new DirectionInput();
+        this.directionInput.init();
+        this.startGameLoop();
 
 
     }
