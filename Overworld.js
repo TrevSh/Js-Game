5b938df40246796f816ec6e695644630cedee570
Tre <trevonshelby@outlook.com>
2022-06-01 14:42:52 -0500

    doBehaviorEvent

--- Overworld.js
+++ Overworld.js
@@ -53,11 +53,11 @@ class Overworld {
         this.startGameLoop();
 
         this.map.startCutscene([
-            {who: "hero", type: "walk", direction: "left"},
-            {who: "hero", type: "walk", direction: "left"},
-            {who: "npcA", type: "walk", direction: "right"},
-            {who: "npcA", type: "walk", direction: "right"},
-            {who: "npcA", type: "stand", direction: "down", time: 800},
+            {who: "hero", type: "walk", direction: "down"},
+            {who: "hero", type: "walk", direction: "down"},
+            {who: "npcA", type: "walk", direction: "left"},
+            {who: "npcA", type: "walk", direction: "left"},
+            {who: "npcA", type: "stand", direction: "up", time: 800},
         ])
 
 
